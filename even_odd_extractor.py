1b746b7afd24f9483649d18a109ef33fc062ac9e
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-22 16:36:03 +0800

    Pseudocode

---
+++ even_odd_extractor.py
@@ -0,0 +1,12 @@
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt that will 
+# ontains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt 
+# that will contains all odd numbers extracted from the numbers.txt.
+
+
+# Open the input file and read the numbers
+# Convert the numbers to integers
+# Extract even and odd numbers
+# Write even numbers to even.txt
+# Write odd number to odd.txt
+# Display the confirmation message
