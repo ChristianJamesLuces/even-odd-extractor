0a790def45be343e1cab6c5a74738f25789d9e56
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-23 22:48:16 +0800

    rewriting the some words

--- even_odd_extractor.py
+++ even_odd_extractor.py
@@ -1,6 +1,6 @@
 # Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
 # The program will create two other text file; the first text file will be named even.txt that will 
-# ontains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt 
+# Contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt 
 # that will contains all odd numbers extracted from the numbers.txt.
 
 import pyfiglet
