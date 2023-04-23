90193f6f7f19662b0fa8a58eb86197e5d60c585f
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-23 19:39:12 +0800

    defining variables

--- even_odd_extractor.py
+++ even_odd_extractor.py
@@ -3,7 +3,10 @@
 # ontains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt 
 # that will contains all odd numbers extracted from the numbers.txt.
 
-
+import pyfiglet
+import time
+# Defining variables
+intro = pyfiglet.figlet_format("WELCOME".center(39, "="), font = "digital")
 # Open the input file and read the numbers
 with open("numbers.txt", "r") as input_file:
     numbers = input_file.readlines()
@@ -12,12 +15,14 @@ numbers = [int(num) for num in numbers]
 # Extract even and odd numbers
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
-# Write even numbers to even.txt
-with open("even.txt", "w") as even_file:
+
+with open("even.txt", "w") as even_file, open("odd.txt", "w") as odd_file:
+    even_file.write("These are the even numbers in the file:" + "\n")
+    # Write even numbers to even.txt
     for num in even_numbers:
-        even_numbers.write(str(num) + "\n")
-# Write odd number to odd.txt
-with open("odd.text", "w") as odd_file:
+        even_file.write(str(num) + "\n")
+    # Write odd number to odd.txt
+    odd_file.write("These are the odd numbers in the file:" + "\n")
     for num in odd_numbers:
-        odd_numbers.write(str(num) + "\n")
+        odd_file.write(str(num) + "\n")
 # Display the confirmation message
